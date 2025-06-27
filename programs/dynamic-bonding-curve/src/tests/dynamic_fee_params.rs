6beaf6f7069e9218ad8f9291553d0fc028aa5068
defi0x1 <cristiando0902@gmail.com>
2025-06-27 17:21:30 +0700

    add more test

--- programs/dynamic-bonding-curve/src/tests/dynamic_fee_params.rs
+++ programs/dynamic-bonding-curve/src/tests/dynamic_fee_params.rs
@@ -2,14 +2,17 @@ use crate::{
     calculate_dynamic_fee_params,
     constants::{
         dynamic_fee::{
-            BIN_STEP_BPS_DEFAULT, BIN_STEP_BPS_U128_DEFAULT, MAX_VOLATILITY_ACCUMULATOR,
-            SQUARE_VFA_BIN,
+            BIN_STEP_BPS_DEFAULT, BIN_STEP_BPS_U128_DEFAULT, MAX_DYNAMIC_FEE_PERCENT,
+            MAX_VOLATILITY_ACCUMULATOR, SQUARE_VFA_BIN,
         },
+        fee::FEE_DENOMINATOR,
         BASIS_POINT_MAX, ONE_Q64,
     },
     params::fee_parameters::to_numerator,
+    state::{fee::VolatilityTracker, DynamicFeeConfig},
     PoolError,
 };
+
 use proptest::prelude::*;
 
 proptest! {
@@ -52,4 +55,56 @@ fn test_calculate_dynamic_fee_params() {
 
     assert_eq!(max_volatility_accumulator, MAX_VOLATILITY_ACCUMULATOR);
     assert_eq!(square_vfa_bin, SQUARE_VFA_BIN);
+}
+
+#[test]
+pub fn test_validate_dynamic_fee_params() {
+    let base_fee_bps = 1000;
+    let base_fee_numerator = to_numerator(base_fee_bps, FEE_DENOMINATOR.into()).unwrap();
+    let dynamic_fee_params = calculate_dynamic_fee_params(base_fee_numerator).unwrap();
+
+    let dynamic_fee_config = DynamicFeeConfig {
+        initialized: 1,
+        max_volatility_accumulator: dynamic_fee_params.max_volatility_accumulator,
+        variable_fee_control: dynamic_fee_params.variable_fee_control,
+        bin_step: dynamic_fee_params.bin_step,
+        filter_period: dynamic_fee_params.filter_period,
+        decay_period: dynamic_fee_params.decay_period,
+        reduction_factor: dynamic_fee_params.reduction_factor,
+        bin_step_u128: dynamic_fee_params.bin_step_u128,
+        ..Default::default()
+    };
+
+    let volatility_tracker = VolatilityTracker {
+        volatility_accumulator: u128::from(dynamic_fee_params.max_volatility_accumulator),
+        ..Default::default()
+    };
+
+    let dynamic_fee_numerator = dynamic_fee_config
+        .get_variable_fee_numerator(&volatility_tracker)
+        .unwrap();
+
+    let expect_dynamic_fee_numerator = u128::from(base_fee_numerator)
+        .checked_mul(MAX_DYNAMIC_FEE_PERCENT.into())
+        .unwrap()
+        .checked_div(100)
+        .unwrap();
+
+    println!(
+        "dynamic_fee_numerator: {} - expect_dynamic_fee_numerator {}",
+        dynamic_fee_numerator, expect_dynamic_fee_numerator
+    );
+
+    let diff = (expect_dynamic_fee_numerator)
+        .checked_sub(dynamic_fee_numerator)
+        .unwrap()
+        .checked_mul(100)
+        .unwrap()
+        .checked_div(expect_dynamic_fee_numerator)
+        .unwrap();
+
+    println!("diff {}", diff);
+
+    // less than 1%
+    assert!(diff < 1);
 }
