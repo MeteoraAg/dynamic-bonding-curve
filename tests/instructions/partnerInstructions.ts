4c04b4921721e5605eb39c9060a6f7dc116e9105
codewithgun <codewithgun@gmail.com>
2025-11-14 23:05:31 +0800

    fix: test fn name

--- tests/instructions/partnerInstructions.ts
+++ tests/instructions/partnerInstructions.ts
@@ -207,7 +207,7 @@ export async function createDammV2OnlyConfig(
   const config = Keypair.generate();
 
   const transaction = await program.methods
-    .createDammv2Config({
+    .createConfigForDammv2Migration({
       ...instructionParams,
       padding: new Array(64).fill(new BN(7)),
     })
