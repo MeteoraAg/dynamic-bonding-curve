cb8dd8325fffbd983b321591672734ad6879fc6c
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2025-12-30 10:34:46 +0700

    fix typo

--- programs/dynamic-bonding-curve/src/math/safe_math.rs
+++ programs/dynamic-bonding-curve/src/math/safe_math.rs
@@ -129,7 +129,7 @@ macro_rules! try_into_impl {
                     Ok(result) => Ok(result),
                     Err(_) => {
                         let caller = Location::caller();
-                        msg!("Typecast is failed at {}:{}", caller.file(), caller.line());
+                        msg!("TypeCast is failed at {}:{}", caller.file(), caller.line());
                         Err(PoolError::TypeCastFailed)
                     }
                 }
