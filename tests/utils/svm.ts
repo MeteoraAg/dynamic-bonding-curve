75c52f200e0e4cfee5eb507619d36f0aef552f08
bangyro <229454856+bangyro@users.noreply.github.com>
2026-01-21 01:21:16 +0800

    feat: add test

--- tests/utils/svm.ts
+++ tests/utils/svm.ts
@@ -8,9 +8,11 @@ import {
   DAMM_V2_PROGRAM_ID,
   DYNAMIC_BONDING_CURVE_PROGRAM_ID,
   FLASH_RENT_FUND,
+  JUPITER_V6_PROGRAM_ID,
   LOCKER_PROGRAM_ID,
   METAPLEX_PROGRAM_ID,
   VAULT_PROGRAM_ID,
+  ZAP_PROGRAM_ID,
 } from "./constants";
 
 export function startSvm() {
@@ -24,12 +26,16 @@ export function startSvm() {
   const sourceFileAlphaVaultPath = path.resolve("./tests/fixtures/vault.so");
   const sourceFileLockerPath = path.resolve("./tests/fixtures/locker.so");
   const sourceFileMetaplexPath = path.resolve("./tests/fixtures/metaplex.so");
+  const sourceFileZapProgramPath = path.resolve("./tests/fixtures/zap.so");
+  const sourceFileJupiterPath = path.resolve("./tests/fixtures/jupiter.so");
   svm.addProgramFromFile(DYNAMIC_BONDING_CURVE_PROGRAM_ID, sourceFileDbcPath);
   svm.addProgramFromFile(DAMM_V2_PROGRAM_ID, sourceFileDammV2Path);
   svm.addProgramFromFile(DAMM_PROGRAM_ID, sourceFileDammV1Path);
   svm.addProgramFromFile(VAULT_PROGRAM_ID, sourceFileAlphaVaultPath);
   svm.addProgramFromFile(LOCKER_PROGRAM_ID, sourceFileLockerPath);
   svm.addProgramFromFile(METAPLEX_PROGRAM_ID, sourceFileMetaplexPath);
+  svm.addProgramFromFile(ZAP_PROGRAM_ID, sourceFileZapProgramPath);
+  svm.addProgramFromFile(JUPITER_V6_PROGRAM_ID, sourceFileJupiterPath);
 
   // set wrap sol mint account
   svm.setAccount(NATIVE_MINT, {
