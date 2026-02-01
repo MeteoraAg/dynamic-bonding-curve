8dff647d608f19a4a5012b9da915ce17a0dbfaef
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2026-02-01 10:51:42 +0800

    bump migration fee

--- programs/dynamic-bonding-curve/src/constants.rs
+++ programs/dynamic-bonding-curve/src/constants.rs
@@ -103,8 +103,8 @@ pub mod fee {
     // max pool creation fee: 100 SOL
     pub const MAX_POOL_CREATION_FEE: u64 = 100_000_000_000;
 
-    // migration fee bps, temporarily set to zero, it could be increased one day
-    pub const PROTOCOL_LIQUIDITY_MIGRATION_FEE_BPS: u16 = 0; // 0%
+    // migration fee bps
+    pub const PROTOCOL_LIQUIDITY_MIGRATION_FEE_BPS: u16 = 20; // 0.2%
 }
 
 pub mod seeds {
