ca954f24f40d7ab26e690f4b152a0e026f29ecc2
bangyro <229454856+bangyro@users.noreply.github.com>
2026-04-21 16:36:57 +0800

    refactor: move process create_config into its own file

--- programs/dynamic-bonding-curve/src/instructions/partner/create_config/ix_create_transfer_hook_config.rs
+++ programs/dynamic-bonding-curve/src/instructions/partner/create_config/ix_create_transfer_hook_config.rs
@@ -1,10 +1,12 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{token_2022, token_interface::Mint};
 
+#[allow(deprecated)]
+use crate::event::{EvtCreateConfig, EvtCreateConfigV2WithTransferHook};
+#[allow(deprecated)]
 use crate::{
-    event::EvtCreateConfigV2WithTransferHook,
     state::{ConfigWithTransferHook, TokenType},
-    PoolError,
+    CreateConfigResult, PoolError,
 };
 
 use super::{process_create_config, ConfigParameters};
@@ -60,7 +62,13 @@ pub fn handle_create_config_with_transfer_hook(
     );
 
     let mut config = ctx.accounts.config.load_init()?;
-    process_create_config(
+    let CreateConfigResult {
+        swap_base_amount,
+        included_protocol_fee_migration_base_amount,
+        fixed_token_supply_flag,
+        pre_migration_token_supply,
+        post_migration_token_supply,
+    } = process_create_config(
         &mut config,
         &config_parameters,
         &ctx.accounts.quote_mint,
@@ -68,6 +76,38 @@ pub fn handle_create_config_with_transfer_hook(
         ctx.accounts.leftover_receiver.key,
     )?;
     config.transfer_hook_program = ctx.accounts.transfer_hook_program.key();
+
+    #[allow(deprecated)]
+    {
+        emit_cpi!(EvtCreateConfig {
+            config: ctx.accounts.config.key(),
+            fee_claimer: ctx.accounts.fee_claimer.key(),
+            quote_mint: ctx.accounts.quote_mint.key(),
+            owner: ctx.accounts.leftover_receiver.key(),
+            pool_fees: config_parameters.pool_fees.clone(),
+            collect_fee_mode: config_parameters.collect_fee_mode,
+            migration_option: config_parameters.migration_option,
+            activation_type: config_parameters.activation_type,
+            token_decimal: config_parameters.token_decimal,
+            token_type: config_parameters.token_type,
+            partner_permanent_locked_liquidity_percentage: config_parameters
+                .partner_permanent_locked_liquidity_percentage,
+            partner_liquidity_percentage: config_parameters.partner_liquidity_percentage,
+            creator_permanent_locked_liquidity_percentage: config_parameters
+                .creator_permanent_locked_liquidity_percentage,
+            creator_liquidity_percentage: config_parameters.creator_liquidity_percentage,
+            swap_base_amount,
+            migration_quote_threshold: config_parameters.migration_quote_threshold,
+            migration_base_amount: included_protocol_fee_migration_base_amount,
+            sqrt_start_price: config_parameters.sqrt_start_price,
+            fixed_token_supply_flag,
+            pre_migration_token_supply,
+            post_migration_token_supply,
+            locked_vesting: config_parameters.locked_vesting,
+            migration_fee_option: config_parameters.migration_fee_option,
+            curve: config_parameters.curve.clone(),
+        });
+    }
 
     emit_cpi!(EvtCreateConfigV2WithTransferHook {
         config: ctx.accounts.config.key(),
