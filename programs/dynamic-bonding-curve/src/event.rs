67cf14803a26103374df2d8d18c87bea50a1625a
bangyro <229454856+bangyro@users.noreply.github.com>
2026-04-24 14:30:49 +0800

    refactor: move macros to macros file

--- programs/dynamic-bonding-curve/src/event.rs
+++ programs/dynamic-bonding-curve/src/event.rs
@@ -9,25 +9,6 @@ use crate::{
     ConfigParameters, LockedVestingParams, SwapParameters, SwapParameters2,
 };
 
-/// Defines two event structs with different names but the same properties
-macro_rules! define_event_pair {
-    (
-        $name:ident, $name_pair:ident {
-            $($field:ident : $ty:ty),* $(,)?
-        }
-    ) => {
-        #[event]
-        pub struct $name {
-            $(pub $field: $ty),*
-        }
-
-        #[event]
-        pub struct $name_pair {
-            $(pub $field: $ty),*
-        }
-    };
-}
-
 /// Create partner metadata
 #[event]
 pub struct EvtPartnerMetadata {
