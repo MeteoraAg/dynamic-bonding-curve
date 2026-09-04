44219e2efdce86f74cad4a1319c47b65bbd43db1
bangyro <229454856+bangyro@users.noreply.github.com>
2026-09-04 12:25:17 +0800

    fix: typo

--- programs/dynamic-bonding-curve/src/instructions/migration/dynamic_amm_v2/migrate_damm_v2_initialize_pool.rs
+++ programs/dynamic-bonding-curve/src/instructions/migration/dynamic_amm_v2/migrate_damm_v2_initialize_pool.rs
@@ -125,7 +125,7 @@ pub struct MigrateDammV2Ctx<'info> {
     pub system_program: Program<'info, System>,
     // Remaining accounts:
     // 0. [READ-ONLY] damm v2 config account
-    // A DBC token badge is only valid within DBC, DAMMv2 uses its own token badge
+    // A DBC token badge is only valid within DBC, damm-v2 uses its own token badge
     // If the quote mint is not permissionless-supported by damm-v2, the damm-v2 config must have the `CreatePoolWithoutMintValidation` permission.
 }
 
